93dbef959fe2ebaef4ea55e45921ccee34721f5c
zlewns <zlewns@gmail.com>
2022-03-14 14:20:04 +0900

    Added Mypage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Mypage from'./pages/Mypage';
+import Calendar from './pages/Calender'
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button>로그인</button>
-        <button>회원가입</button>
-        <button>마이페이지</button>
+      <BrowserRouter>
+        <Mypage />
+      </BrowserRouter>,
       </header>
     </div>
   );
