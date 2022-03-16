5766dc9a53e0b530a1b129fa3b43711b45db60a7
JIS <ygc8257@naver.com>
2022-03-16 14:48:54 +0900

    0316JIS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,10 @@
-import Register from './pages/Register';
-import Mypage from './pages/Mypage';
-import Login from './pages/Login';
+import Map from './components/Map';
 import React from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
-
 
 const App = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/login">로그인</Link>
-        </li>
-        <li>
-          <Link to="/mypage">마이페이지</Link>
-        </li>
-        <li>
-          <Link to="/signin">회원가입</Link>
-        </li>
-      </ul>
-      <hr />
-      <Routes>
-        <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/mypage" element={<Mypage/>} />
-        <Route exact path="/signin" element={<Register/>} />
-      </Routes>  
+      <Map/>
     </div>
   );
 };
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -9,12 +9,32 @@ const Map=()=>{
         level: 3
       };
       var map = new kakao.maps.Map(container, options);
+      var marker = new kakao.maps.Marker({ 
+        // 지도 중심좌표에 마커를 생성합니다 
+        position: map.getCenter() 
+      }); 
+      // 지도에 마커를 표시합니다
+      marker.setMap(map);
+      kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+    
+        // 클릭한 위도, 경도 정보를 가져옵니다 
+        var latlng = mouseEvent.latLng;
+
+        marker.setPosition(latlng);
+        
+        var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+        message += '경도는 ' + latlng.getLng() + ' 입니다';
+        
+        var resultDiv = document.getElementById('result'); 
+        resultDiv.innerHTML = message;
+        });
       }, [])
   
   
       return (
           <div>
-              <div id="map" style={{width:"500px", height:"400px"}}></div> 
+              <div id="map" style={{width:"100vw", height:"95vh"}}></div>
+              <p id ="result">asd</p>
           </div>
       )
   }
