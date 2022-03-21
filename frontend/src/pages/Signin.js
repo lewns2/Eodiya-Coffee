e44f717f51d62d4651d84442ee4c3bcf3bcc809b
조영현 <loveyh95@gmail.com>
2022-03-21 17:22:12 +0900

    frontend/signup and login

--- frontend/src/pages/Signin.js
+++ frontend/src/pages/Signin.js
@@ -1,52 +1,69 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
-const Register =(props) =>{
+import React from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+const Register = (props) => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => {
-    axios.post( '/accounts/signup', 
-      data,
-      { 
-        headers:{ 
-          'Content-type': 'application/json', 
-          'Accept': 'application/json' 
-        } 
-      } 
-      ) 
-      .then((response) => { handleCloseModal()}) 
-      .catch((response) => { console.log('Error!') });
-    }
-  const handleCloseModal = () =>{
+  const onSubmit = (data) => {
+    axios
+      .post("/accounts/signup/", data, {
+        headers: {
+          "Content-type": "application/json",
+          Accept: "*/*",
+        },
+      })
+      .then((response) => {
+        handleCloseModal();
+      })
+      .catch((response) => {
+        console.log("Error!");
+        console.log(response);
+      });
+  };
+  const handleCloseModal = () => {
     props.setSigninIsOpen(false);
-  } 
+  };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label>닉네임
-        <input {...register("user_nickname", { required: true, maxLength: 20 })} />
+      <label>
+        닉네임
+        <input {...register("username", { required: true, maxLength: 20 })} />
       </label>
-      <br/>
-      <label>이메일
-        <input {...register("user_email", { pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })} />
+      <br />
+      <label>
+        이메일
+        <input
+          {...register("email", {
+            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+          })}
+        />
       </label>
-      <br/>
-      <label>비밀번호
+      <br />
+      <label>
+        비밀번호
         <input {...register("password", { maxLength: 20 })} />
       </label>
-      <br/>
-      <label>생일
+      <label>
+        비밀번호 확인
+        <input {...register("passwordConfirmation", { maxLength: 20 })} />
+      </label>
+      <br />
+      <label>
+        생일
         <input {...register("birthday", { maxLength: 20 })} />
       </label>
-      <br/>
-      <label>남
-        <input {...register("gender", {  })} type="radio" value="male" />
+      <br />
+      <label>
+        남
+        <input {...register("gender", {})} type="radio" value="male" />
       </label>
-      <label>여
-        <input {...register("gender", {  })} type="radio" value="female" />
+      <label>
+        여
+        <input {...register("gender", {})} type="radio" value="female" />
       </label>
-      <br/>
+      <br />
       <input type="submit" />
     </form>
   );
-}
+};
 
 export default Register;
