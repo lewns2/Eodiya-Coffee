f54b341d7f68effd009683c9b122f80d658c2a3f
zlewns <zlewns@gmail.com>
2022-03-28 10:39:45 +0900

    [add] MUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Nav from './components/Nav';
 import Map from './components/Map';
 import './styles/App.css'
-
+import { Button } from '@mui/material';
 
 const App = () => {
   return (
     <div>
       <Nav/>
+      <Button>Hello World</Button>
       <Map/>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
+
 axios.defaults.baseURL = "http://127.0.0.1:8000";
 axios.defaults.withCredentials = false;
 axios.defaults.xsrfCookieName = "csrftoken";
