8b1f49a99830fa42523667f5a65064262a95dabf
chlaudwo930 <chlaudwo930@naver.com>
2022-04-05 11:52:20 +0900

    cafe json
    Former-commit-id: 1a3329cb65ba10352cea22f884ce2df375b5404a

--- server/cafes/serializers.py
+++ server/cafes/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import CafeList, CafeMenu
+from commercial_area.serializers import CommercialAreaSerializer
 
 class CafeListSerializer(serializers.ModelSerializer):
+    commercialCode = CommercialAreaSerializer(many=True, read_only=True)
     class Meta:
         model = CafeList
-        fields = '__all__'
+        fields = ('dongCode', 'commercialCode', 'guName', 'UrlId', 'cafeName', 'cafeRate',
+                  'reviewCount', 'cafeAddress', 'cafeHour', 'cafeTel', 'cafeHomepage', 'cafeTag', 
+                  'cafePhoto', 'cafePoint'
+                  )
 
 class CafeMenuSerializer(serializers.ModelSerializer):
     
