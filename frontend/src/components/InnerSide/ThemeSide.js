0cdf3eec84cd15dd41b42e1d5642ec8dacc4afa4
JIS <ygc8257@naver.com>
2022-04-07 02:37:21 +0900

    0407JIS
    Former-commit-id: 63d74c5618ff62721ca0778db51a2d5cc321e649

--- frontend/src/components/InnerSide/ThemeSide.js
+++ frontend/src/components/InnerSide/ThemeSide.js
@@ -8,18 +8,17 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useSelector } from "react-redux";
 import Button from '@mui/material/Button';
+import "../../styles/ThemeSide.css";
 import Chart from "react-apexcharts";
 
 const ThemeSide = () => {
     const themeNum = useSelector(state => state.setMap.eodiyaMap.themeNum);
     const themeGuData = useSelector(state => state.setMap.eodiyaMap.themeGuData);
-    const[series, setSeries] = useState([
-        {
+    const [series, setSeries] = useState([{
           name: "세대별 이용수 평균",
           data: [0, 0, 0, 0, 0]
-        }
-      ]);
-    const[options, setOptions] = useState({
+        }]);
+      const[options, setOptions] = useState({
         chart: {
           id: "basic-bar"
         },
@@ -30,8 +29,55 @@ const ThemeSide = () => {
     const moveLoc= (index) =>{
         console.log(index);
     }
+    const resetData = () =>{
+        if (themeNum == 0){
+            setSeries([{
+                name: "저녁 총매출(17-24시)",
+                data: [themeGuData[0].revenue_in_night, themeGuData[1].revenue_in_night, themeGuData[2].revenue_in_night, themeGuData[3].revenue_in_night, themeGuData[4].revenue_in_night]
+            }])
+        }else if(themeNum == 1){
+            setSeries([{
+                name: "생활인구 1명 당 매출액",
+                data: [themeGuData[0].revenue_per_one_person, themeGuData[1].revenue_per_one_person, themeGuData[2].revenue_per_one_person, themeGuData[3].revenue_per_one_person, themeGuData[4].revenue_per_one_person]
+            }])
+        }else if(themeNum == 2){
+            setSeries([{
+                name: "상권 주변 회사원 수",
+                data: [themeGuData[0].salarymanNumber, themeGuData[1].salarymanNumber, themeGuData[2].salarymanNumber, themeGuData[3].salarymanNumber, themeGuData[4].salarymanNumber]
+            }])
+        }else if(themeNum == 3){
+            setSeries([{
+                name: "11~14시 생활인구",
+                data: [themeGuData[0].revenue1114, themeGuData[1].revenue1114, themeGuData[2].revenue1114, themeGuData[3].revenue1114, themeGuData[4].revenue1114]
+            }])
+        }else if(themeNum == 4){
+            setSeries([{
+                name: "상권 주변 학교들의 수(초등학교 + 유치원)",
+                data: [themeGuData[0].schoolTotal, themeGuData[1].schoolTotal, themeGuData[2].schoolTotal, themeGuData[3].schoolTotal, themeGuData[4].schoolTotal]
+            }])
+        }else if(themeNum == 5){
+            setSeries([{
+                name: "10대와 20대 생활인구 수",
+                data: [themeGuData[0].sum1020, themeGuData[1].sum1020, themeGuData[2].sum1020, themeGuData[3].sum1020, themeGuData[4].sum1020]
+            }])
+        }else if(themeNum == 6){
+            setSeries([{
+                name: "생활인구 1명 당 매출액",
+                data: [themeGuData[0].revenue_per_one_person, themeGuData[1].revenue_per_one_person, themeGuData[2].revenue_per_one_person, themeGuData[3].revenue_per_one_person, themeGuData[4].revenue_per_one_person]
+            }])
+        }else if(themeNum == 7){
+            setSeries([{
+                name: "10대와 20대 생활인구 수",
+                data: [themeGuData[0].peopleTotal, themeGuData[1].peopleTotal, themeGuData[2].peopleTotal, themeGuData[3].peopleTotal, themeGuData[4].peopleTotal]
+            }])
+        }else{
+            setSeries([{
+                name: "세대별 이용수 평균",
+                data: [10, 20, 30, 40, 50] 
+            }])
+        }
+    }
     useEffect(()=>{
-        var op = [];
         if(themeGuData.length != 0){
             setOptions(
                 {
@@ -43,89 +89,45 @@ const ThemeSide = () => {
                     },
                 }
             )
-            if (themeNum == 0){
-                setSeries([{
-                    name: "저녁 총매출(17-24시)",
-                    data: [themeGuData[0].revenue_in_night, themeGuData[1].revenue_in_night, themeGuData[2].revenue_in_night, themeGuData[3].revenue_in_night, themeGuData[4].revenue_in_night]
-                }])
-            }else if(themeNum == 1){
-                setSeries([{
-                    name: "생활인구 1명 당 매출액",
-                    data: [themeGuData[0].revenue_per_one_person, themeGuData[1].revenue_per_one_person, themeGuData[2].revenue_per_one_person, themeGuData[3].revenue_per_one_person, themeGuData[4].revenue_per_one_person]
-                }])
-            }else if(themeNum == 2){
-                setSeries([{
-                    name: "상권 주변 회사원 수",
-                    data: [themeGuData[0].salarymanNumber, themeGuData[1].salarymanNumber, themeGuData[2].salarymanNumber, themeGuData[3].salarymanNumber, themeGuData[4].salarymanNumber]
-                }])
-            }else if(themeNum == 3){
-                setSeries([{
-                    name: "11~14시 생활인구",
-                    data: [themeGuData[0].revenue1114, themeGuData[1].revenue1114, themeGuData[2].revenue1114, themeGuData[3].revenue1114, themeGuData[4].revenue1114]
-                }])
-            }else if(themeNum == 4){
-                setSeries([{
-                    name: "상권 주변 학교들의 수(초등학교 + 유치원)",
-                    data: [themeGuData[0].schoolTotal, themeGuData[1].schoolTotal, themeGuData[2].schoolTotal, themeGuData[3].schoolTotal, themeGuData[4].schoolTotal]
-                }])
-            }else if(themeNum == 5){
-                setSeries([{
-                    name: "10대와 20대 생활인구 수",
-                    data: [themeGuData[0].sum1020, themeGuData[1].sum1020, themeGuData[2].sum1020, themeGuData[3].sum1020, themeGuData[4].sum1020]
-                }])
-            }else if(themeNum == 6){
-                setSeries([{
-                    name: "생활인구 1명 당 매출액",
-                    data: [themeGuData[0].revenue_per_one_person, themeGuData[1].revenue_per_one_person, themeGuData[2].revenue_per_one_person, themeGuData[3].revenue_per_one_person, themeGuData[4].revenue_per_one_person]
-                }])
-            }else if(themeNum == 7){
-                setSeries([{
-                    name: "10대와 20대 생활인구 수",
-                    data: [themeGuData[0].peopleTotal, themeGuData[1].peopleTotal, themeGuData[2].peopleTotal, themeGuData[3].peopleTotal, themeGuData[4].peopleTotal]
-                }])
-            }
         }
     }, [themeGuData]);
     return (
-        <div>
-            {themeNum == 5 &&
-            <Fragment>
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 500 }} aria-label="simple table">
-                        <TableHead>
-                        <TableRow>
-                            <TableCell>순위</TableCell>
-                            <TableCell align="right">지역</TableCell>
-                            <TableCell align="right">시장성</TableCell>
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 500 }} aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>순위</TableCell>
+                        <TableCell align="right">지역</TableCell>
+                        <TableCell align="right">이동</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {themeGuData.map((recommRows, index) => (
+                        <TableRow
+                        key={index}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell component="th" scope="row">
+                            {index+1}
+                        </TableCell>
+                        <TableCell align="right">{recommRows.commercialAreaName}</TableCell>
+                        <TableCell align="right"><Button onClick={()=>moveLoc(index)}>이동</Button></TableCell>
                         </TableRow>
-                        </TableHead>
-                        <TableBody>
-                        {themeGuData.map((recommRows, index) => (
-                            <TableRow
-                            key={index}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                            <TableCell component="th" scope="row">
-                                {index+1}
-                            </TableCell>
-                            <TableCell align="right">{recommRows.commercialAreaName}</TableCell>
-                            <TableCell align="right"><Button onClick={()=>moveLoc(index)}>이동</Button></TableCell>
-                            </TableRow>
-                        ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <div className='m-Y' id="chart">
-                    <Chart
-                    options={options}
-                    series={series}
-                    type="bar"
-                    width={520}
-                    />
-                </div>
-            </Fragment>
-        }
-    </div>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <div className='bgw'>
+                <Chart
+                options={options}
+                series={series}
+                type="bar"
+                width={520}
+                />
+                <Button onClick={()=>resetData()}>그래프 표시</Button>
+            </div>
+        </>
     );
 }
 
