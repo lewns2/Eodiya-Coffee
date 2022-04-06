bf0eb3102b4a9e79e9077deb4e3ada0adeeaeb1c
조영현 <loveyh95@gmail.com>
2022-04-07 02:06:06 +0900

    modify recommendation.urls
    Former-commit-id: 5913a4b18a856e6654f038479323699653978aaf

--- server/recommendation/urls.py
+++ server/recommendation/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    # path('recommend/', views.recommend),
     path('recommend/kids/<str:gu_name>/', views.kid_recommend),
     path('recommend/study/<str:gu_name>/', views.study_recommend),
     path('recommend/play/<str:gu_name>/', views.play_recommend),
@@ -12,5 +11,4 @@ urlpatterns = [
     path('recommend/machine/<str:gu_name>/', views.machine_recommend),
     path('recommend/dessert/<str:gu_name>/', views.dessert_recommend),
     path('recommend/study/<str:gu_name>/', views.study_recommend),
-    # path('recommend/<str:gu_name>/', views.recommend)
 ]
