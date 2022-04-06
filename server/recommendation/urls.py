fa05e3d13360e852d9fd007691e607fb71b69e91
Jin Seong <gh0jhs@naver.com>
2022-04-07 00:17:21 +0900

    upload
    Former-commit-id: 010dd32892117722916b679ee60a9932b3ec3dca

--- server/recommendation/urls.py
+++ server/recommendation/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('recommend/kids/<str:gu_name>/', views.kid_recommend),
     path('recommend/study/<str:gu_name>/', views.study_recommend),
     path('recommend/play/<str:gu_name>/', views.play_recommend),
+    path('recommend/brunch/<str:gu_name>/', views.brunch_recommend),
     # path('recommend/brunch/<str:gu_name>/', views.recommend)
     # path('recommend/<str:gu_name>/', views.recommend)
 ]
