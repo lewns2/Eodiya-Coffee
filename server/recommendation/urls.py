de12e1c2f59fddd8a5f202af7686b3e63622e0bd
조영현 <loveyh95@gmail.com>
2022-04-06 21:23:12 +0900

    merge merge
    Former-commit-id: bafdcbfe9aa338dfb72d525ccf99a90177edc705

--- server/recommendation/urls.py
+++ server/recommendation/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('recommend/study/<str:gu_name>/', views.study_recommend)
-    # path('recommend/brunch/<str:gu_name>/', views.recommend)
+    path('recommend/study/<str:gu_name>/', views.study_recommend),
+    path('recommend/dessert/<str:gu_name>/', views.dessert_recommend),
     # path('recommend/<str:gu_name>/', views.recommend)
 ]
