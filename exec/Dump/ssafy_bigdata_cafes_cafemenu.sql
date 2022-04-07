df1bc3527a630aa66c1100b03b6b2b1dc6e2e6a9
Jin Seong <gh0jhs@naver.com>
2022-04-08 02:42:28 +0900

    upload exec
    Former-commit-id: 868c50afc49956702d189707226e529e48013604

---
+++ exec/Dump/ssafy_bigdata_cafes_cafemenu.sql
@@ -0,0 +1,45 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: j6e203.p.ssafy.io    Database: ssafy_bigdata
+-- ------------------------------------------------------
+-- Server version	8.0.28-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cafes_cafemenu`
+--
+
+DROP TABLE IF EXISTS `cafes_cafemenu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cafes_cafemenu` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `menuName` varchar(255) DEFAULT NULL,
+  `menuPrice` int DEFAULT NULL,
+  `UrlId_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cafes_cafemenu_UrlId_id_6a58f049_fk_cafes_cafelist_UrlId` (`UrlId_id`),
+  CONSTRAINT `cafes_cafemenu_UrlId_id_6a58f049_fk_cafes_cafelist_UrlId` FOREIGN KEY (`UrlId_id`) REFERENCES `cafes_cafelist` (`UrlId`)
+) ENGINE=InnoDB AUTO_INCREMENT=54685 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-07 22:18:21
